093b193445d2236e898d09ffadb3baca85b6c0b0
minh lam <tranminhlam0323@gmail.com>
2018-11-30 18:32:11 +0700

    BTL

---
+++ Project/assets/js/main.js
@@ -0,0 +1,183 @@
+"use strict";
+jQuery(document).ready(function ($) {
+
+    $(window).load(function () {
+        $(".loaded").fadeOut();
+        $(".preloader").delay(1000).fadeOut("slow");
+    });
+
+     // * Mobile menu
+
+    $('#bs-example-navbar-collapse-1').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 0)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+     // * WOW
+
+    var wow = new WOW({
+        mobile: false 
+    });
+    wow.init();
+
+
+
+     // Carousel
+
+
+    $('.main_home_slider').owlCarousel({
+        responsiveClass: true,
+        autoplay: false,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        navText: [
+            "<i class='lnr lnr-chevron-left'></i>",
+            "<i class='lnr lnr-chevron-right'></i>"
+        ],
+        autoplayHoverPause: true
+
+    });
+
+    $('.single_features_slide').owlCarousel({
+        responsiveClass: true,
+        autoplay: false,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        navText: [
+            "<i class='lnr lnr-chevron-left'></i>",
+            "<i class='lnr lnr-chevron-right'></i>"
+        ],
+        autoplayHoverPause: true
+
+    });
+
+    $('.main_teastimonial_slider').owlCarousel({
+        responsiveClass: true,
+        autoplay: false,
+        items: 1,
+        loop: true,
+        dots: true,
+        nav: false,
+        navText: [
+            "<i class='lnr lnr-chevron-left'></i>",
+            "<i class='lnr lnr-chevron-right'></i>"
+        ],
+        autoplayHoverPause: true
+
+    });
+
+
+// magnificPopup
+
+    $('.portfolio-img').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+//skillbar section
+
+    var skillBarTopPos = jQuery('.skillbar').position().top;
+    jQuery(document).scroll(function () {
+        var scrolled_val = $(document).scrollTop().valueOf();
+        if (scrolled_val > skillBarTopPos - 250)
+            startAnimation();
+    });
+
+    function startAnimation() {
+        jQuery('.skillbar').each(function () {
+            jQuery(this).find('.skillbar-bar').animate({
+                width: jQuery(this).attr('data-percent')
+            }, 6000);
+        });
+    }
+    ;
+
+
+// Counter 
+
+    $('.statistic-counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+// main-menu-scroll
+
+    jQuery(window).scroll(function () {
+        var top = jQuery(document).scrollTop();
+        var height = 300;
+
+        if (top > height) {
+            jQuery('.navbar-fixed-top').addClass('menu-scroll');
+        } else {
+            jQuery('.navbar-fixed-top').removeClass('menu-scroll');
+        }
+    });
+
+// scroll Up
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+//    $('#menu').slicknav();
+    jQuery('#portfoliowork').mixItUp({
+        selectors: {
+            target: '.tile',
+            filter: '.filter',
+            sort: '.sort-btn'
+        },
+        animation: {
+            animateResizeContainer: false,
+            effects: 'fade scale'
+        }
+
+    });
+
+
+
+    $('.dropdown-menu').click(function (e) {
+        e.stopPropagation();
+    });
+
+
+});
+
+
+
+$(document).on("scroll", function () {
+    if ($(document).scrollTop() > 120) {
+        $("nav").addClass("small");
+    } else {
+        $("nav").removeClass("small");
+    }
+});
+
+
+
